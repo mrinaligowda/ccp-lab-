882a972ab08a4b80e705d5fee53358ed260dc6f0
mrinaligowda <85470921+mrinaligowda@users.noreply.github.com>
2021-06-08 23:37:17 +0530

    Lab2 Programs

---
+++ Lab2(17:05)/Extra Programs Lab 2/ASCII.c
@@ -0,0 +1,9 @@
+//Program to display ASCII value of a given character 
+#include<stdio.h>
+int main(){
+	char ch ;
+	printf("Enter a Character for which you want ASCII value\n");
+	scanf("%c",&ch);
+	printf("ASCII value of the given Character %c is %d",ch,ch );
+	return 0;
+}
